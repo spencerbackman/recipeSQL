0d06784e56e634635cc39d37624feae323e6a85e
spencerbackman <backmanspencer99@gmail.com>
2021-06-06 13:46:05 -0500

    connected to heroku postgres

--- queries.js
+++ queries.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const recipeRouter = express.Router();
 require("dotenv").config();
+const { Client } = require('pg');
 
-const Pool = require('pg').Pool;
-const pool = new Pool({
-  user: 'spencer',
-  host: 'localhost',
-  database: 'recipehub',
-  password: process.env.PASSWORD,
-  port: 5432
-})
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+// const Pool = require('pg').Pool;
+// const pool = new Pool({
+//   user: 'spencer',
+//   host: 'localhost',
+//   database: 'recipehub',
+//   password: process.env.PASSWORD,
+//   port: 5432
+// })
 
 recipeRouter.get('/', (req, res) => {
-  pool.query('SELECT * FROM recipes', (err, results) => {
+  client.query('SELECT * FROM recipes', (err, results) => {
     if(err) {
       throw err;
     }
@@ -22,7 +32,7 @@ recipeRouter.get('/', (req, res) => {
 
 recipeRouter.get('/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    pool.query('SELECT ingredientsToRecipes.ing_id, ingredientsToRecipes.rec_id, ingredientsToRecipes.quantity, ingredientsToRecipes.quantity_unit, ingredients.name FROM ingredientsToRecipes JOIN ingredients ON ingredientsToRecipes.ing_id = ingredients.ing_id WHERE ingredientsToRecipes.rec_id = $1', [id])
+    client.query('SELECT ingredientsToRecipes.ing_id, ingredientsToRecipes.rec_id, ingredientsToRecipes.quantity, ingredientsToRecipes.quantity_unit, ingredients.name FROM ingredientsToRecipes JOIN ingredients ON ingredientsToRecipes.ing_id = ingredients.ing_id WHERE ingredientsToRecipes.rec_id = $1', [id])
       .then(results => {
         console.log(results.rows[0])
         res.status(200).send(results)
@@ -43,17 +53,17 @@ recipeRouter.post('/', (req, res) => {
   const description = req.body.recipe.description;
   const ingredients = req.body.recipe.ingredients;
   console.log('INGREDIENTS!!!!!!!     ' + ingredients )
-  pool.query('INSERT INTO recipes (name, by, description) VALUES ($1, $2, $3) RETURNING id', [name, by, description])
+  client.query('INSERT INTO recipes (name, by, description) VALUES ($1, $2, $3) RETURNING id', [name, by, description])
     .then(res => {
       console.log(res.rows[0].id)
       const rec_id = res.rows[0].id;
       let ing_id;
       ingredients.map(item => {
-        pool.query('INSERT INTO ingredients (name) VALUES ($1) RETURNING ing_id', [item.name])
+        client.query('INSERT INTO ingredients (name) VALUES ($1) RETURNING ing_id', [item.name])
           .then(res => {
             console.log(res.rows[0].ing_id)
             ing_id = res.rows[0].ing_id;
-            pool.query('INSERT INTO ingredientsToRecipes (quantity, quantity_unit, rec_id, ing_id) VALUES ($1, $2, $3, $4)', [item.quantity, item.unit, rec_id, ing_id])
+            client.query('INSERT INTO ingredientsToRecipes (quantity, quantity_unit, rec_id, ing_id) VALUES ($1, $2, $3, $4)', [item.quantity, item.unit, rec_id, ing_id])
               .then(res => console.log(res.rows[0]))
               .catch(err => console.error('Error executing query', err.stack))
           })
@@ -95,62 +105,62 @@ recipeRouter.post('/', (req, res) => {
   // })
 })
 
-const getRecipes = (req, res) => {
-  pool.query('SELECT * FROM recipe', (err, results) => {
-    if(err) {
-      console.log(err)
-      throw err;
-    }
-    res.status(200).send(results);
-  })
-}
-
-const getRecipe = (req, res) => {
-  const id = parseInt(req.params.id)
-  pool.query('SELECT * FROM recipe WHERE id = $1', [id], (err, results) => {
-    if(err) {
-      throw err;
-    }
-    response.status(200).json(results.rows)
-  })
-}
-
-
-const createRecipe = (req, res) => {
-  const { name, by, date, cuisine } = req.body;
-
-  pool.query('INSERT INTO recipe (name, by, date, cuisine) VALUES ($1, $2, $3, $4)',
-  [name, by, date, cuisine],
-  (err, results) => {
-    if(err) {
-      throw err;
-    }
-    response.status(201).send(`Recipe added with ID: ${result.insertId}`)
-  })
-}
-
-const updateRecipe = (req, res) => {
-  const id = parseInt(req.params.id);
-  const { name, by, date, cuisine } = req.body;
-
-  pool.query('UPDATE recipes SET name = $1, by = $2, date = $3, cuisine = $4, id = $5',
-    [name, by, date, cuisine, id], (err, results) => {
-      if(err) {
-        throw err;
-      }
-      response.status(200).send(`User modified with ID: ${id}`)
-    })
-}
-
-const deleteRecipe = (req, res) => {
-  const id = parseInt(req.params.id);
-
-  pool.query('DELETE FROM recipes WHERE id = $1', [id], (err, results) => {
-    if(err) {
-      throw err;
-    }
-    response.status(200).send(`User deleted with ID: ${id}`)
-  })
-}
+// const getRecipes = (req, res) => {
+//   pool.query('SELECT * FROM recipe', (err, results) => {
+//     if(err) {
+//       console.log(err)
+//       throw err;
+//     }
+//     res.status(200).send(results);
+//   })
+// }
+//
+// const getRecipe = (req, res) => {
+//   const id = parseInt(req.params.id)
+//   pool.query('SELECT * FROM recipe WHERE id = $1', [id], (err, results) => {
+//     if(err) {
+//       throw err;
+//     }
+//     response.status(200).json(results.rows)
+//   })
+// }
+//
+//
+// const createRecipe = (req, res) => {
+//   const { name, by, date, cuisine } = req.body;
+//
+//   pool.query('INSERT INTO recipe (name, by, date, cuisine) VALUES ($1, $2, $3, $4)',
+//   [name, by, date, cuisine],
+//   (err, results) => {
+//     if(err) {
+//       throw err;
+//     }
+//     response.status(201).send(`Recipe added with ID: ${result.insertId}`)
+//   })
+// }
+//
+// const updateRecipe = (req, res) => {
+//   const id = parseInt(req.params.id);
+//   const { name, by, date, cuisine } = req.body;
+//
+//   pool.query('UPDATE recipes SET name = $1, by = $2, date = $3, cuisine = $4, id = $5',
+//     [name, by, date, cuisine, id], (err, results) => {
+//       if(err) {
+//         throw err;
+//       }
+//       response.status(200).send(`User modified with ID: ${id}`)
+//     })
+// }
+//
+// const deleteRecipe = (req, res) => {
+//   const id = parseInt(req.params.id);
+//
+//   pool.query('DELETE FROM recipes WHERE id = $1', [id], (err, results) => {
+//     if(err) {
+//       throw err;
+//     }
+//     response.status(200).send(`User deleted with ID: ${id}`)
+//   })
+// }
 
 module.exports = recipeRouter;
